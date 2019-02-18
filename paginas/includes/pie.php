6df394daef2ec932832222bebb981d5cf525d1e3
mianduranoc <mianduranoc@ittepic.edu.mx>
2019-02-18 08:09:57 -0700

    Tercer commit

--- paginas/includes/pie.php
+++ paginas/includes/pie.php
@@ -1,7 +1,7 @@
 <!-- inicio del footer -->
 <footer>
 				<div class="contacto">
-					<h3>Contacto</h3>
+					<h3>Holi</h3>
 					<ul>
 						<li><a href="#">Correo: contacto@elmetronomo.com</a></li>
 						<li>Telefono: 311 123 44 55</li>
